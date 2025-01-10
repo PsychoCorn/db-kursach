9d117f10cb74ead646c6b3497080a79f16ba39d3
PsychoCorn <icetime.artem@gmail.com>
2025-01-10 17:37:17 +0800

    123

--- src/authorization.rs
+++ src/authorization.rs
@@ -1,19 +1,41 @@
 use postgres::{Client, NoTls};
+use tables::group;
 use super::CONNECTION;
 use super::*;
 use std::hash::{DefaultHasher, Hash, Hasher};
 
 #[derive(Debug, Clone)]
+pub(super) struct  Group {
+    pub specialization_id: i64,
+    pub specialization_name: String,
+    pub id: i64,
+    pub year: i64,
+    pub number: i64,
+    pub cifr: String
+}
+
+#[derive(Debug, Clone)]
+pub(super) struct  FullName {
+    pub first_name: String,
+    pub second_name: String,
+    pub middle_name: String
+}
+
+#[derive(Debug, Clone)]
 pub(super) struct Student {
     pub login: String,
     pub number: i32,
+    pub full_name: FullName,
+    pub group: Group
 }
 
 impl Student {
-    pub fn new(login: &str, number: i32) -> Self {
+    pub fn new(login: &str, number: i32, full_name: FullName, group: Group) -> Self {
         Self {
             login: login.to_owned(),
-            number
+            number,
+            full_name,
+            group
         }
     }
 }
@@ -21,12 +43,14 @@ impl Student {
 #[derive(Debug, Clone)]
 pub(super) struct Teacher {
     pub login: String,
+    pub subjects: Vec<i64>
 }
 
 impl Teacher {
-    pub fn new(login: &str) -> Self {
+    pub fn new(login: &str, subjects: Vec<i64>) -> Self {
         Self {
-            login: login.to_owned()
+            login: login.to_owned(),
+            subjects
         }
     }
 }
@@ -55,8 +79,33 @@ pub(super) fn check_user(login: &str, password: &str) -> Result<User, postgres::
         match id_role {
             1 => Ok(User::Admin),
             2 => Ok(User::Decan),
-            3 => Ok(User::Student(Student::new(row[0].get(0), 0))),
-            4 => Ok(User::Teacher(Teacher::new(row[0].get(0)))),
+            3 => {
+                let login: &str = row[0].get(0);
+                let query = format!("select * from get_student_info('{}');", login);
+                let row = client.query(&query, &[])?;
+                
+                Ok(User::Student(Student::new(
+                    login, 
+                    row[0].get(0),
+                    FullName {
+                        first_name: row[0].get(1),
+                        second_name: row[0].get(2),
+                        middle_name: row[0].get(3),
+                    },
+                    Group {
+                        id: row[0].get(4),
+                        cifr: row[0].get(5),
+                        year: row[0].get(6),
+                        number: row[0].get(7),
+                        specialization_id: row[0].get(8),
+                        specialization_name: row[0].get(9),
+                    }
+                )))
+            },
+            4 => {
+                let login: &str = row[0].get(0);
+                Ok(User::Teacher(Teacher::new(login, vec![])))
+            },
             _ => unreachable!()
         }
     }
